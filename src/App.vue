4b0352826760f8f3da178671836fa2e114cd152f
tom lee <tdroid99@gmail.com>
2018-08-25 17:28:13 -0600

    have working header/footer/Profiles

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="app">
     <Header/>
+    <Profiles v-bind:dinosArray="dinos"/>
     <Footer/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Profiles from './components/Profiles'
 import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Profiles,
     Footer
-  }
+  },
+  
 }
 </script>
 
---
+++ src/components/Profiles.vue
@@ -0,0 +1,47 @@
+<template>
+    <main>
+        <section id="profiles-container">
+            <h2>Profiles</h2>
+            <ul id="profiles">
+                <Card v-for="dino in dinosArray" :key='dino.id' :dino='dino' />
+            </ul>
+        </section>
+    </main>
+</template>
+
+<script>
+import Card from './Card'
+
+export default {
+    name: 'Profiles',
+    components: {
+        Card
+    },
+    props:['dinosArray']
+}
+</script>
+
+<style>
+main {
+    grid-row: 2/3;
+}
+
+main section {
+    padding: 0 20px 0 20px;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, x100);
+    grid-gap: 10px;
+    margin: 0 auto;
+    width: 50%;
+}
+
+small {
+    color: black;
+    font-size: .5rem;
+    margin-left: 10px;
+}
+
+p {
+    font-size: .8rem;
+}
+</style>
